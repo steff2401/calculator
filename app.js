9c7701286051b3be20a8abdbdf274aa8c46e43b5
steff2401 <stephank@hotmail.no>
2023-07-11 14:01:56 +0200

    Add function for the four operators

---
+++ app.js
@@ -0,0 +1,19 @@
+
+function addition(num1, num2) {
+    return num1+num2;
+}
+
+function subtraction(num1, num2) {
+    return num1-num2;
+}
+
+function multiplication(num1, num2) {
+    return num1*num2;
+}
+
+function division(num1, num2) {
+    return num1/num2;
+}
+
+
+
