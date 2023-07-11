da126572550af1f55126ba84a10f8a72fbdaa620
steff2401 <stephank@hotmail.no>
2023-07-11 14:06:27 +0200

    Add function to retunr result of operation

--- app.js
+++ app.js
@@ -15,5 +15,29 @@ function division(num1, num2) {
     return num1/num2;
 }
 
+let num1;
+let operator;
+let num2;
 
+function operate(num1, operator, num2) {
+
+    switch (operator) {
+        
+        case "+":
+            return addition(num1, num2);
+            break;
+
+        case "-":
+            return subtraction(num1, num2);
+            break;
+
+        case "x":
+            return multiplication(num1, num2);
+            break;
+
+        case "/":
+            return division(num1, num2);
+            break;
+    }
+}
 
